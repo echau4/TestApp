a272741aeb6a29dfc3ba09f0c3757f9ac4f94cd7
William Shepherd <williamrshepherd@gmail.com>
2018-06-10 17:14:29 -0500

    Create app component

---
+++ components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+// App component - represents the whole app
+
+export default class App extends Component {
+  render() {
+    return (
+      <p>Hello, world!</p>
+    );
+  }
+}
