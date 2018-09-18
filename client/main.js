59f698e79a5dbbc46292f9afa20a9490b39408eb
echau4 <31198647+echau4@users.noreply.github.com>
2018-09-18 09:44:56 -0500

    Trying to display the list of names in the database on html page automatically
    Needs to be fixed because it also returns nothing. When you change method to Names.find().count() in client/main.js it shows 0 data is in the database (perhaps its seraching up the minimongo?)

--- client/main.js
+++ client/main.js
@@ -4,7 +4,16 @@ import { render } from "react-dom";
 
 import "./main.html";
 import App from "../app/App";
+import Names from "../import/api/database";
 
 Meteor.startup(() => {
   render(<App />, document.getElementById("render-target"));
 });
+
+if(Meteor.isClient){
+  Template.form.helpers({
+    people: function(){
+      return Names.find();
+    }
+  });
+}
